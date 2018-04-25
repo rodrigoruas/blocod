7847c2f797518305283765250dd095f09cf92e1d
Rodrigo Ruas <rodrigoruas2@gmail.com>
2018-04-25 10:53:27 -0300

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_scope :user do
+     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+     get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+   end
+
+  devise_for :users
+
   get 'home', to: "pages#home"
-
-  get 'bebo', to: "pages#bebo"
-
   get 'contact', to: "pages#contact"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"
 
 end
